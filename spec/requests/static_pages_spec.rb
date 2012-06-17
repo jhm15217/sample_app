c60fc84eb4140577e2fc0d5d263a19dad75e7d51
James Morris <james.morris@cmu.edu>
2012-06-16 23:09:19 -0400

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,13 +4,19 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  shared_examples_for "all static pages" do
+     it { should have_selector('h1',    text: heading) }
+     it { should have_selector('title', text: full_title(page_title)) }
+   end
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
-  end
+   describe "Home page" do
+     before { visit root_path }
+     let(:heading)    { 'Sample App' }
+     let(:page_title) { '' }
+
+     it_should_behave_like "all static pages"
+     it { should_not have_selector 'title', text: '| Home' }
+   end
 
   describe "Help page" do
     before { visit help_path }
